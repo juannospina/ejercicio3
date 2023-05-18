bdb347fc485263f4b6198c49ec19234eb0112a66
Juan Ospina <jeol3739@gmail.com>
2023-05-18 14:15:33 -0500

    Ejercicio 3 Java Intro

---
+++ src/ejercicio3/Ejercicio3.java
@@ -0,0 +1,19 @@
+
+package ejercicio3;
+
+public class Ejercicio3 {
+    public static void main(String[] args) {
+        int num1, num2, suma, resta;
+        num1 = 5;
+        num2 = 10;
+        
+        suma = num1 + num2;
+        num1++;
+        
+        System.out.println("La suma de ambos números es de: " +suma+ " y el incremento del número 1 es: " + num1);
+        
+        
+        
+    }
+    
+}
